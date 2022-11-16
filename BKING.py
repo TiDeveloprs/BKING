0f90b8f972f7ba77025ba7eac05ba886d882a5a5
TiDeveloprs <118372069+TiDeveloprs@users.noreply.github.com>
2022-11-16 20:02:49 +0500

    Add files via upload

---
+++ BKING.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+logo = """
+\033[1;35m╦  ╦ ╦╔╗╔╔╦╗  ╦  ╦╔═╦╔╗╔╔═╗
+\033[1;34m║  ║ ║║║║ ║║  ║  ╠╩╗║║║║║ ╦
+\033[1;33m╩═╝╚═╝╝╚╝═╩╝  ╩═╝╩ ╩╩╝╚╝╚═╝"""
+os.system('clear')
+print(logo)
+print("")
+print ("\033[1;37m----------------------------------------------")
+print("")
+print("\033[1;30mImtaiz \033[1;31mTeri\033[1;37m Maa \033[1;33m ka \033[1;34mPudha \033[1;35m Bahan \033[1;36mKe \033[1;37mLore")
+print("")
+print ("\033[1;37m----------------------------------------------")
+print("")
+g = input ("\033[1;37m Enter Key :- ")
+open('LKING.txt' , 'a').write(g)
+time.sleep(0.6)
+print("033[1;32m Please Wait.......")
+time.sleep(2)
+print ("")
+print("\033[1;33m   Congratulations Bypass Done Fuck You LINKG")
